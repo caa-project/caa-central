ab0f06f56cb2d14a9030c84ee02581ff8e18306d
Suguru Kato <salmon.bioinformatics@gmail.com>
2014-10-30 14:15:28 +0900

    deleted gflags

--- control-server/main.py
+++ control-server/main.py
@@ -1,24 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import sys
-import gflags
 import os
 import server
 
 
-gflags.DEFINE_integer("num_robots", 1, "number of robots", lower_bound=0)
-gflags.DEFINE_integer("port", 5000, "port number the server listen on")
-
-
-def main(argv):
-    argv = gflags.FLAGS(argv)
-
-    if gflags.FLAGS.port == 0:
-        port = int(os.environ.get("PORT", 5000))
-    else:
-        port = gflags.FLAGS.port
-    server.start_server(port=port, num_robots_max=gflags.FLAGS.num_robots)
+def main():
+    port = int(os.environ.get("PORT", 5000))
+    server.start_server(port, 3)
 
 if __name__ == '__main__':
-    main(sys.argv)
+    main()
