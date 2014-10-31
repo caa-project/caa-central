31b2db7e0b2f4cfa7854d3b04f6e7eb7110b51dd
Suguru Kato <salmon.bioinformatics@gmail.com>
2014-10-31 23:51:11 +0900

    added gflags

--- ui-server/main.py
+++ ui-server/main.py
@@ -2,14 +2,25 @@
 # -*- coding: utf-8 -*-
 
 import sys
+import gflags
 import os
 import ui_server
 
 
+gflags.DEFINE_integer("port", None, "port number the server listen on")
+gflags.DEFINE_string("control_server_url",
+        "http://benijake-caa-control-server.herokuapp.com", "url of the control server")
+
+
 def main(argv):
-    port = int(os.environ.get("PORT", 5000))
+    argv = gflags.FLAGS(argv)
+    if gflags.FLAGS.port is None: # default value
+        port = int(os.environ.get("PORT", 5000))
+    else:
+        port = gflags.FLAGS.port
     control_server_url = 'http://benijake-caa-control-server.herokuapp.com'
-    ui_server.start_server(port=port, control_server_url=control_server_url)
+    ui_server.start_server(port=port,
+            control_server_url=gflags.FLAGS.control_server_url)
 
 if __name__ == '__main__':
     main(sys.argv)
